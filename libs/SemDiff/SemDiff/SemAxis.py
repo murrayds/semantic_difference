fbca5c42bac4c1167a38c3fbda14532eedffcab2
murrayds <dakota.s.murray@gmail.com>
2019-04-07 22:15:20 -0400

    Incorperated the base SemAxis code

--- libs/SemDiff/SemDiff/SemAxis.py
+++ libs/SemDiff/SemDiff/SemAxis.py
@@ -10,7 +10,7 @@ def cosine_similarity(vec1, vec2):
 def get_avg_vec(emb, word, k):
     tmp = []
     tmp.append(emb.wv[word])
-    for closest_word, similarity in emb.most_similar(positive=word, topn=k):
+    for closest_word, similarity in emb.wv.most_similar(positive=word, topn=k):
         tmp.append(emb.wv[closest_word])
     avg_vec = np.mean(tmp, axis=0)
     return avg_vec
@@ -33,29 +33,12 @@ def project_word_on_axis(emb, word, antonym, k=10):
 # actual embedding process, jsut for testing purposes
 def test_perform_embedding(embedding_path):
     import gensim
-    EMBEDDING_PATH = embedding_path ### Yout path to embedding file
+    # Define the path to the embedding folder
+    embed = gensim.models.KeyedVectors.load_word2vec_format(embedding_path)
+    result = project_word_on_axis(embed, "baby", ("hate", "love"), k=3)
+    print(result)
 
-    ######################################################################
-    ### Google News embedding (Download: https://code.google.com/archive/p/word2vec/). Note that for SemAxis, bin file needs to be converted to text file: see https://stackoverflow.com/questions/27324292/convert-word2vec-bin-file-to-text)
-
-    # test_path = "%s/GoogleNews-vectors-negative300.txt" % (EMBEDDING_PATH)
-    ######################################################################
-
-    ######################################################################
-    ## Reddit20M embedding (Download: https://drive.google.com/file/d/1ewmS5Uu4tWAkwWsuY8FZVgLr85vvZXye/view?usp=sharing)
-
-    test_path = "%s/Reddit20M.cbow.300.100.txt" % (EMBEDDING_PATH)
-
-    test_embed = gensim.models.KeyedVectors.load_word2vec_format(test_path)
-
-    print(project_word_on_axis(test_embed, "baby", ("hate", "love"), k=3))
 
 if __name__ == '__main__':
-
-    EMBEDDING_PATH = "" ### Yout path to embedding file
-
-    test_perform_embedding(EMBEDDING_PATH)
-
-    ## Test results (with k=3) should be:
-    ## 0.16963434219360352 with Google News embedding
-    ## 0.31472429633140564 with Reddit20M embedding
+    path  = '/Users/dakotamurray/Documents/semantic_difference/data/SandersForPresident.cbow.300.0.005.10.txt'
+    test_perform_embedding(path)
